4a03e8e4a2bc54780232157393e3b4333ac59e4c
Shasherina Alexandra <shasherina.alexandra@gmail.com>
2020-06-29 18:35:06 +0300

    выносим minmax в заголовочный файл

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "histogram.h"
 
 
 using namespace std;
@@ -13,23 +14,6 @@ vector<double> input_numbers(const size_t count)
     }
 
     return result;
-
-}
-void find_minmax(const vector<double>& numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    for(double numb: numbers)
-    {
-        if (numb < min)
-        {
-            min = numb;
-        }
-        if (numb > max)
-        {
-            max = numb;
-        }
-    }
 
 }
 
